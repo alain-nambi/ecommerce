d7c4afdf606b2b12ff20c48f070d03a71bf17fe7
alain <alainnambi@gmail.com>
2022-08-29 09:07:23 +0300

    Add and configure tailwind CSS to phoenix application

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :ecommerce, EcommerceWeb.Endpoint,
   secret_key_base: "aAdN53bZiDhG3ji1eTLbu6Jgb7aeVY91grRUNU8HtsOoyuaO0r3SBeV9MD5BGYsm",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
