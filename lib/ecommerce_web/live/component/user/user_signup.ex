5a6505f0cc91d49eb768488828bba1259fe70065
alain <alainnambi@gmail.com>
2022-08-31 14:35:35 +0300

    Turn the user registration template into a component

---
+++ lib/ecommerce_web/live/component/user/user_signup.ex
@@ -0,0 +1,9 @@
+defmodule EcommerceWeb.LiveComponent.User.UserSignup do
+  use Phoenix.LiveComponent
+
+  alias EcommerceWeb.UserView
+
+  def render(assigns) do
+    UserView.render("user_signup.html", assigns)
+  end
+end
